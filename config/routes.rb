a565c1b6a316810c3d9f6fe39bf8dc6f94086d41
qleml <cchristoph@student.ethz.ch>
2024-09-17 21:40:04 +0200

    first db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "exercises#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/exercises", to: "exercises#index"
 
