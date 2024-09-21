3b450af02aab94a6ad8db011a64f066c605ed0af
qleml <cchristoph@student.ethz.ch>
2024-09-21 17:53:15 +0200

    exercise_type is now optional

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,6 +1,5 @@
 class Exercise < ApplicationRecord
-  belongs_to :exercise_type
+  belongs_to :exercise_type, optional: true
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :exercise_type, presence: true
 end
--- app/models/exercise_type.rb
+++ app/models/exercise_type.rb
@@ -1,3 +1,3 @@
 class ExerciseType < ApplicationRecord
-  has_many :exercises  # Ensure this line exists
+  has_many :exercises
 end
