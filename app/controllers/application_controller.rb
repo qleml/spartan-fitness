c64c7b8252d7246c120088fa6a29f2c01aefbc90
qleml <cchristoph@student.ethz.ch>
2024-09-24 08:52:30 +0200

    feat: logged in header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
-  
   helper_method :current_selected_user
 
   before_action :ensure_user_selected
