78e6a97f94cd5907b0a7d58adf3f6700fd9ccc7d
qleml <cchristoph@student.ethz.ch>
2024-09-25 09:26:29 +0200

    refactor: sleeker naming

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
-  helper_method :current_selected_user
+  helper_method :current_user
 
   before_action :ensure_user_selected
 
-  def current_selected_user
-    @current_selected_user ||= User.find_by(id: session[:user_id])
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def ensure_user_selected
-    unless current_selected_user
+    unless current_user
       redirect_to select_user_path unless request.path == select_user_path
     end
   end
